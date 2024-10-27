b702ff3e5c07114aadd3f1d21ba3799044b4827b
patilaalap <patilaalap@gmail.com>
2024-10-26 20:36:00 -0400

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -20,9 +22,9 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            """
+
             # Do this if you have a raw data file
-            df = pd.read_csv('') # Get raw data
+            df = pd.read_csv('/Users/aalap/mlproject/data/train.csv') # Get raw data
             logging.info('Read the dataset as dataframe')
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -30,11 +32,13 @@ class DataIngestion:
             
             logging.info("Train test split initiated")
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
-            """
 
+            """
             logging.info('Read the dataset as dataframe')
             train_set = pd.read_csv('/Users/aalap/mlproject/data/train.csv')
             test_set = pd.read_csv('/Users/aalap/mlproject/data/test.csv')
+            """
+
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
@@ -48,6 +52,10 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
 
+
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
